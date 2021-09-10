5daa7da54fcf89d0bcdb126eb9ad7817277b345b
elric97 <sharma.rachit882@gmail.com>
2021-09-10 00:33:25 -0400

    initializes repository

---
+++ src/main/java/com/se21/calbot/CalBotApplication.java
@@ -0,0 +1,13 @@
+package com.se21.calbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/se21/calbot/CalBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.se21.calbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
