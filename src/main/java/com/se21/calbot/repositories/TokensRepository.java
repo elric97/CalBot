c12d1c0b542c8a4b1ac8908359c9c1d6ec503f1e
elric97 <sharma.rachit882@gmail.com>
2021-09-25 11:21:59 -0400

    Adds postgres as database

--- src/main/java/com/se21/calbot/repositories/TokensRepository.java
+++ src/main/java/com/se21/calbot/repositories/TokensRepository.java
@@ -1,9 +1,9 @@
 package com.se21.calbot.repositories;
 
 import com.se21.calbot.model.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TokensRepository extends MongoRepository<User, String> {
+public interface TokensRepository extends JpaRepository<User, String> {
 }
